3a4cd0bef1a05c15ac4a0fe464bb127e1e3b8938
Henry Philpotts <hphilpotts@gmail.com>
2022-10-25 14:58:09 +0100

    Added UPDATE and DELETE User APIs, testing in Postman.

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,68 @@
 const { User } = require('../models/User');
+const { Pokegotchi } = require('../models/Pokegotchi')
 
+// -- CREATE:
+
+    // N/A for User, see auth.js!
+
+// -- READ:
+
+// HTTP GET - Index of Users:
 exports.user_index_get = (req, res) => {
     User.find()
     .then(users => {
-        res.json({users})
+        res.json({users});
     })
     .catch(err => {
-        console.log(err)
+        console.log(err);
     })
 }
 
+// HTTP GET - User Detail by Id:
 exports.user_detail_get = (req, res) => {
-    console.log('finding user')
+    console.log('finding user');
     User.findById(req.query.id)
     .then(user => {
-        res.json({user})
+        res.json({user});
     })
     .catch(err => {
-        console.log(err)
+        console.log(err);
+    })
+}
+
+// -- UPDATE:
+
+// HTTP GET - Get Edit User Page by Id:
+exports.user_edit_get = (req, res) => {
+    User.findById(req.query.id)
+    .then(user => {
+        res.json({user});
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+// HTTP PUT - Edit User and Updated:
+exports.user_edit_put = (req, res) => {
+    User.findByIdAndUpdate(req.body._id, req.body, {new: true})
+    .then(user => {
+        res.json({user});
+    })
+    .catch(err => {
+        console.log(err);
+    })   
+}
+
+// -- DELETE:
+
+// HTTP DELETE - Delete User by Id:
+exports.user_delete = (req, res) => {
+    User.findByIdAndDelete(req.query.id).populate('pokegotchi')
+    .then(user => {
+        res.json({user});
+    })
+    .catch(err => {
+        console.log(err);
     })
 }
