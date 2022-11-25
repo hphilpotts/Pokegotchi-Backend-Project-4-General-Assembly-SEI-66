8306ca3a12a3d2ca3c90c4239ff10e40f2f34b78
Henry Philpotts <hphilpotts@gmail.com>
2022-11-25 19:35:01 +0000

    Updated server.js so that header includes allow PUT etc.

--- server.js
+++ server.js
@@ -77,6 +77,10 @@ app.use((req, res, next) => {
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
     );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'PUT, POST, GET, DELETE, OPTIONS'
+    );
     next();
   });
 
